ffaf44c969671062302fbedd0dd49290af3d9dca
AshLight <>
2025-12-11 17:31:51 +0900

    빌드 경로

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.BASE_PATH || '';
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : '',
+  trailingSlash: true
 };
 
 module.exports = nextConfig;
